7e424686bb26032ce32f5a400ed2350a7cfa2c6c
devferx <ferkki@hotmail.com>
2020-10-18 10:52:55 -0400

    imagen agregada

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Image, View} from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <View>
       <Header />
+      <Image
+        style={styles.imagen}
+        source={require('./assets/img/cryptomonedas.png')}
+      />
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%',
+  },
+});
